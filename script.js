d9090756f258d23555bb4030081bbc415a55f02f
rliedmeiergrowth-create <rliedmeier.growth@gmail.com>
2025-10-02 17:07:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optional future interactivity
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for reaching out! I'll get back to you soon.");
+});
